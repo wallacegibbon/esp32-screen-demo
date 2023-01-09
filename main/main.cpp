511d1defb014a6a1dafc4b27288a0094314c78e8
wallacegibbon <wallacegibbon@aliyun.com>
2023-01-09 18:00:51 +0800

    Trivial changes: `esp_err_t` is being used now.

--- main/main.cpp
+++ main/main.cpp
@@ -8,7 +8,9 @@
 screen::Screen_SSD1306_IIC scr1(I2C_NUM_0, 0x3C, 128, 64);
 
 void i2c_device_init() {
+  esp_err_t ret;
   i2c_config_t config;
+
   config.mode = I2C_MODE_MASTER;
   config.sda_io_num = GPIO_NUM_41;
   config.scl_io_num = GPIO_NUM_40;
@@ -17,16 +19,27 @@ void i2c_device_init() {
   config.master.clk_speed = 400000;
   config.clk_flags = 0;
 
-  i2c_param_config(I2C_NUM_0, &config);
-  i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+  ret = i2c_param_config(I2C_NUM_0, &config);
+  ESP_ERROR_CHECK(ret);
+
+  ret = i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+  ESP_ERROR_CHECK(ret);
 }
 
 void spi_device_init(spi_device_handle_t *dev) {
+  esp_err_t ret;
   spi_bus_config_t bus_config;
+
   bus_config.mosi_io_num = GPIO_NUM_3;
   bus_config.miso_io_num = GPIO_NUM_3;
   bus_config.sclk_io_num = GPIO_NUM_5;
-  spi_bus_initialize(SPI2_HOST, &bus_config, SPI_DMA_DISABLED);
+  bus_config.quadwp_io_num = -1;
+  bus_config.quadhd_io_num = -1;
+  bus_config.max_transfer_sz = 160 * 80 * 2 + 8;
+  bus_config.flags = 0;
+  bus_config.intr_flags = 0;
+  ret = spi_bus_initialize(SPI2_HOST, &bus_config, SPI_DMA_DISABLED);
+  ESP_ERROR_CHECK(ret);
 
   spi_device_interface_config_t interface_config;
   interface_config.address_bits = 0;
@@ -37,13 +50,14 @@ void spi_device_init(spi_device_handle_t *dev) {
   interface_config.cs_ena_posttrans = 0;
   interface_config.input_delay_ns = 0;
   interface_config.clock_speed_hz = 0;
-  interface_config.spics_io_num = GPIO_NUM_0;
+  interface_config.spics_io_num = GPIO_NUM_4;
   interface_config.flags = SPI_DEVICE_NO_DUMMY;
   interface_config.queue_size = 1;
   interface_config.pre_cb = 0;
   interface_config.post_cb = 0;
 
-  spi_bus_add_device(SPI2_HOST, &interface_config, dev);
+  ret = spi_bus_add_device(SPI2_HOST, &interface_config, dev);
+  ESP_ERROR_CHECK(ret);
 }
 
 static void fancy_display_1() {
